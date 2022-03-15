53f37390c699a88deea7ee8bc51aa1ec5a3caf72
chaizafran97 <82008745+chaizafran97@users.noreply.github.com>
2022-03-15 23:55:51 +0800

    Blog

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from blog.forms import CommentForm
 from blog.models import Post, Comment
 
 def blog_index(request):
@@ -18,9 +19,21 @@ def blog_category(request, category):
 
 def blog_detail(request, pk):
     post = Post.objects.get(pk=pk)
+
+    form = CommentForm()
+    if request.method is 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author = form.cleaned_data["author"],
+                body = form.cleaned_data["body"],
+                post = post
+            )
+            comment.save()
     comments = Comment.objects.filter(post=post)
     context = {
         "post":post,
         "comments":comments,
+        "form":form,
     }
     return render(request, "blog_detail.html", context)
